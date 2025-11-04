be5603f26b5bc1eea32916573fbfa69677372ba6
ramramji545 <ramkanyadevi00.1@gmail.com>
2025-11-04 10:40:43 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -13,8 +13,8 @@ DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://rishimeghwanshi11_d
 MONGO_URL = DATABASE_URL  # For auth system
 
 # Owner and Admin Configuration
-OWNER_ID = int(os.environ.get("OWNER_ID", "8283383116"))
-ADMINS = [int(x) for x in os.environ.get("ADMINS", "8283383116").split()]  # Default to owner ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "8390207194"))
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "8390207194").split()]  # Default to owner ID
 
 # Channel Configuration
 PREMIUM_CHANNEL = "https://t.me/+g5M-RwxYQiJhOWFl"
@@ -59,6 +59,7 @@ Please contact the admin @ItsUGBot to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
